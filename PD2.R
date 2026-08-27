d1d4da541ca9cf8ed87e95439e7f9d82b351b9d5
qinghuan.wang <qinghuan.wang@qilu-pharma.com>
2026-08-27 08:21:25 +0000

    Update Note

--- PD2.R
+++ PD2.R
@@ -11,6 +11,7 @@ ic_deviation <- check_icf_time_deviation(data=raw,
                                          ignore_vars = ic_ignore_vars,
                                          exclude_datasets = exclude_ds
                                          )
+# 结果转换为Dataframe
 pd_2_1_1_ouput <- as_check_df(ic_deviation,
                               check_name = "2.1.1 获得ICF前进行了试验相关操作")
 
@@ -18,5 +19,7 @@ pd_2_1_1_ouput <- as_check_df(ic_deviation,
 ic_without <- check_screen_without_ic(raw,
                                       visit_pattern = "筛选|Screening|screening"
                                       )
+
+# 结果转换为Dataframe
 pd_2_4_1_ouput <- as_check_df(ic_without,
                               check_name = "2.4.1 未签署知情同意书")
